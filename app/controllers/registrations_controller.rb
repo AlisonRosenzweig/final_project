405b6a5d4429da68eec230f8875e68ed99b3412c
Alison Rosenzweig <alisonhrosenzweig@gmail.com>
2014-08-08 13:12:07 -0400

    fixed registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,21 +5,16 @@ class RegistrationsController < Devise::RegistrationsController
 		respond_with self.resource
 	end
 
-	def update
+	# def update
+	# 	@user = User.find(current_user.id)
 
-		account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
+	# 	account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
 
-		# "required for settings form to submit when password is left blank"
-		if account_update_params[:password].blank?
-			account_update_params.delete("password")
-			account_update_params.delete("password_confirmation")
-		end
-
-		@user = User.find(current_user.id)
-		if @user.update_attributes(account_update_params)
-			set_flash_message :notice, :updated
-
-	end
+	# 	# "required for settings form to submit when password is left blank"
+	# 	if account_update_params[:password].blank?
+	# 		account_update_params.delete("password")
+	# 		account_update_params.delete("password_confirmation")
+	# end
 
 
 end
