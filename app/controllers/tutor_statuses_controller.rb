97b736ba5ea34be9f53f64ad3748bb4b9d8e9ffc
Alison Rosenzweig <alisonhrosenzweig@gmail.com>
2014-08-09 14:25:06 -0400

    added @user.save, improved but did not fix the problem entirely - still debugging

--- app/controllers/tutor_statuses_controller.rb
+++ app/controllers/tutor_statuses_controller.rb
@@ -3,12 +3,14 @@ class TutorStatusesController < ApplicationController
 def mark_available
 	@user = current_user
   	@user.available = true
+  	@user.save
   	redirect_to '/'
 end
 
 def mark_unavailable
 	@user = current_user
   	@user.available = false
+  	@user.save
   	redirect_to '/'
 end
 
